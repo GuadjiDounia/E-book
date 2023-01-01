1d453935f6cae37425bfe7cdd1417ef961ced3af
douniagdj <douniaguadji@gmail.com>
2023-01-02 00:05:42 +0100

    ya pas mal de truc ma7abouch ydjou kifkif

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const OpenNav = document.querySelector(".icon1")
-const fermerNav = document.querySelector(".fermer")
-const menuNav = document.querySelector(".menu")
+const OpenNav = document.querySelector(".icon1");
+const fermerNav = document.querySelector(".fermer");
+const menuNav = document.querySelector(".menu");
 
 const Positionmenu = menu.getBoundingClientRect().left;
 
 
 OpenNav.addEventListener("click", () =>{
   if(Positionmenu <0){
-    menu.classList.add("monter")
+    menu.classList.add("monter");
   }
 }
 )
@@ -15,7 +15,23 @@ OpenNav.addEventListener("click", () =>{
 
 fermerNav.addEventListener("click", () =>{
   if(Position:menu <0){
-    menu.classList.remove("monter")
+    menu.classList.remove("monter");
   }
 }
 )
+
+
+
+
+var ProductImg = document.getElementById("ProductImg");
+ var smallimg = document.getElementsByClassName("small-img");
+
+ smallimg[0].onclick = function(){
+  ProductImg.src= smallimg[0].src;
+ } 
+ smallimg[1].onclick = function(){
+  ProductImg.src= smallimg[1].src;
+ }
+ smallimg[2].onclick = function(){
+  ProductImg.src= smallimg[2].src;
+ }
